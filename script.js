36c37fe432912b2a6a3d2df026fea74b239c959a
aroldosoriao <aroldo.soriao@escola.pr.gov.br>
2026-08-21 11:06:06 -0300

    ok

---
+++ script.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentloaded",) () => {
+ // Seleciona o botão de curtida e o ícone de coração
+ const likebtn = document . querySeletor("")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
